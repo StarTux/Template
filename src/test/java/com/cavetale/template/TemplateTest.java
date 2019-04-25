88e110642e383da0bdff6f1ba294a8019f1ffeb9
StarTux <ths.loy@gmail.com>
2019-04-25 08:51:41 +0200

    initial commit

---
+++ src/test/java/com/cavetale/template/TemplateTest.java
@@ -0,0 +1,11 @@
+package com.cavetale.template;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public final class TemplateTest {
+    @Test
+    public void test() {
+        Assert.assertEquals(1, 1);
+    }
+}
