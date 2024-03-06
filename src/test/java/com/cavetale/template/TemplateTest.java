34da8699428e379e558969c22f0110bc41c32505
StarTux <ths.loy@gmail.com>
2024-03-06 16:49:09 +0100

    Test case

---
+++ src/test/java/com/cavetale/template/TemplateTest.java
@@ -0,0 +1,11 @@
+package com.cavetale.template;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public final class TemplateTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
